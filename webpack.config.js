fc1b514b480f12201b6a14b17e4a01108b11dcb9
Morgan Caron <caron.morgan91@gmail.com>
2019-09-24 12:11:33 +0200

    Test CI

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@
 const WebpackConfigGenerator = require("webpack-config-generator");
 
 module.exports = (env, argv) => {
-  return WebpackConfigGenerator({
-    mode: argv.mode,
-    entry: {
-      app: ["./src/ts/app.ts", "./src/sass/style.sass", "./src/css/style.css"]
-    }
-  });
+	return WebpackConfigGenerator({
+		mode: argv.mode,
+		entry: {
+			app: ["./src/ts/app.ts", "./src/sass/style.sass", "./src/css/style.css"]
+		}
+	});
 };
+
+exit(1); // test CI fail
