a85f0f40828da29b3e26ec8cc365517f5e84a9d9
Morgan <caron.morgan91@gmail.com>
2020-03-15 05:38:45 +0100

    Code cleaning

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const WebpackConfigGenerator = require("webpack-config-generator");
+const webpackConfigGenerator = require("webpack-config-generator");
 
 module.exports = (env, argv) => {
-	return WebpackConfigGenerator({
+	return webpackConfigGenerator({
 		mode: argv.mode,
 		entry: {
 			app: ["./src/ts/app.ts", "./src/sass/style.sass", "./src/css/style.css"]
 		},
-		favicon: './src/favicon.png'
+		favicon: "./src/favicon.png"
 	});
 };
