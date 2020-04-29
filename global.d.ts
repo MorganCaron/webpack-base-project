5c5a38750cc8c109078d37909d6550270e71344c
Morgan <caron.morgan91@gmail.com>
2020-04-29 17:34:08 +0200

    Code cleaning

--- global.d.ts
+++ global.d.ts
@@ -1,18 +1,94 @@
-declare module "*.ico"
-declare module "*.png"
-declare module "*.svg"
-declare module "*.jpg"
-declare module "*.jpeg"
-declare module "*.gif"
-declare module "*.webp"
-declare module "*.eot"
-declare module "*.otf"
-declare module "*.ttf"
-declare module "*.woff"
-declare module "*.woff2"
-declare module "*.txt"
-declare module "*.html"
-declare module "!!raw-loader!*" {
-	const contents: string;
-	export default contents;
+declare module '!!raw-loader!*' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.ico' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.png' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.svg' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.jpg' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.jpeg' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.bmp' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.gif' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.webp' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.eot' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.otf' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.ttf' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.woff' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.woff2' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.txt' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.html' {
+	const src: string;
+	export default src;
+}
+
+declare module '*.module.css' {
+	const classes: { readonly [key: string]: string };
+	export default classes;
+}
+
+declare module '*.module.scss' {
+	const classes: { readonly [key: string]: string };
+	export default classes;
+}
+
+declare module '*.module.sass' {
+	const classes: { readonly [key: string]: string };
+	export default classes;
 }
